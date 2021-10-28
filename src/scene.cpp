416f3134adc3102296f08e11367398c21bf223a9
GeorgiadeG <I.GEORGIADES@student.tudelft.nl>
2021-10-28 23:37:29 +0200

    solve errors

--- src/scene.cpp
+++ src/scene.cpp
@@ -91,7 +91,6 @@ Scene loadScene(SceneType type, const std::filesystem::path& dataDir)
         // Spherical light: position, radius, color
         //scene.lights.push_back(SphericalLight{ glm::vec3(0, 1.5f, 0), 0.2f, glm::vec3(1) });
     } break;
-    };
     case Custom2: {
         // === Replace custom.obj by your own 3D model (or call your 3D model custom.obj) ===
         auto subMeshes = loadMesh(dataDir / "tile_scene.obj");
